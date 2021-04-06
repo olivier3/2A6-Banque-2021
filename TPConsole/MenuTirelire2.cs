9fa2d3ca18074c789f39617b7016bd84cb497f22
Olivier Bilodeau <olivierbilodeau@outlook.com>
2021-04-06 14:28:29 -0400

    FIN 2.3 - Réduire la redondance

--- TPConsole/MenuTirelire2.cs
+++ TPConsole/MenuTirelire2.cs
@@ -16,60 +16,8 @@ namespace TPConsole
             do
             {
                 ConsolePlus.Clear();
-                AfficherEntête();
-            } while (TraiterMenuEtContinuer());
-        }
-
-
-        /// <summary>
-        /// Affiche l'entête de la Tirelire
-        /// </summary>
-        private static void AfficherEntête()
-        {
-            ConsolePlus.WriteLine();
-            ConsolePlus.WriteLine(ConsoleColor.Magenta, "OB - Tirelire 2");
-            ConsolePlus.WriteLine(ConsoleColor.Magenta, "===============");
-            ConsolePlus.WriteLine();
-            ConsolePlus.Afficher("Historique", "");
-            ConsolePlus.WriteLine(Historique.ConstructionHistorique());
-            ConsolePlus.WriteLine();
-            ConsolePlus.Afficher("Actif", $"{Tirelire2.MontantTotal:C}");
-            ConsolePlus.WriteLine();
-        }
-
-        /// <summary>
-        /// Actionne une fonction de la tirelire selon le choix de l'utilisateur
-        /// </summary>
-        /// <returns>Détermine si on quitte ou non le programme</returns>
-        private static bool TraiterMenuEtContinuer()
-        {
-            if (ConsolePlus.LireChoix(out string? choix, '0',
-                    "Quitter", "Déposer", "Retirer", "Vider"))
-            {
-                ConsolePlus.WriteLine();
-                switch (choix)
-                {
-                    case "Déposer":
-                        Déposer();
-                        break;
-
-                    case "Retirer":
-                        Retirer();
-                        break;
-
-                    case "Vider":
-                        Vider();
-                        break;
-
-                    case "Quitter":
-                        return false;
-
-                    default:
-                        Debug.Fail($"Cas non traité: {choix}");
-                        break;
-                }
-            }
-            return true;
+                MenuUtil.AfficherEntête(Tirelire2.MontantTotal, 2);
+            } while (MenuUtil.TraiterMenuEtContinuer(Déposer, Retirer, Vider));
         }
 
         /// <summary>
---
+++ TPConsole/MenuUtil.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using cstjean.info.fg.consoleplus;
+
+using TireLireLib;
+
+namespace TPConsole
+{
+    public static class MenuUtil
+    {
+        public static void AfficherEntête(decimal montantTirelire, int numéroTirelire)
+        {
+            ConsolePlus.WriteLine();
+            ConsolePlus.WriteLine(ConsoleColor.Magenta, $"OB - Tirelire {numéroTirelire}");
+            ConsolePlus.WriteLine(ConsoleColor.Magenta, "===============");
+            ConsolePlus.WriteLine();
+            ConsolePlus.Afficher("Historique", "");
+            ConsolePlus.WriteLine(Historique.ConstructionHistorique());
+            ConsolePlus.WriteLine();
+            ConsolePlus.Afficher("Actif", $"{montantTirelire:C}");
+            ConsolePlus.WriteLine();
+        }
+
+        public static bool TraiterMenuEtContinuer(Action déposer, Action retirer, Action vider)
+        {
+            if (ConsolePlus.LireChoix(out string? choix, '0',
+                    "Quitter", "Déposer", "Retirer", "Vider"))
+            {
+                ConsolePlus.WriteLine();
+                switch (choix)
+                {
+                    case "Déposer":
+                        déposer();
+                        break;
+
+                    case "Retirer":
+                        retirer();
+                        break;
+
+                    case "Vider":
+                        vider();
+                        break;
+
+                    case "Quitter":
+                        return false;
+
+                    default:
+                        Debug.Fail($"Cas non traité: {choix}");
+                        break;
+                }
+            }
+            return true;
+        }
+    }
+}
