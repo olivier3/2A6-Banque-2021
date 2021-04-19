bf1d07d794fef6988f79ac9af306bdfbafbb6f39
Olivier Bilodeau <olivierbilodeau@outlook.com>
2021-04-18 20:45:18 -0400

    FIN 8.5 - Sauvegarde robuste

--- TPConsole/Program.cs
+++ TPConsole/Program.cs
@@ -11,7 +11,13 @@ namespace TPConsole
             Console.Title = "OB - TP Banque";
             ConsolePlus.IndentationGénérale = 1;
             Persistance.Charger();
+            AppDomain.CurrentDomain.ProcessExit += ExitHandler;
             MenuGénéral.Afficher();
+            Persistance.Sauvegarder();
+        }
+
+        private static void ExitHandler(object? sender, EventArgs e)
+        {
             Persistance.Sauvegarder();
         }
     }
