a49d3d44918ceef2373862ddafc01f013ddfc19e
Olivier Bilodeau <olivierbilodeau@outlook.com>
2021-04-26 13:47:59 -0400

    FIN 12.1 - État

---
+++ BanqueLib/ÉtatDuCompte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanqueLib
+{
+    public class ÉtatDuCompte
+    {
+        public enum ÉtatCompte
+        {
+            Actif,
+            Fermé
+        }
+    }
+}
